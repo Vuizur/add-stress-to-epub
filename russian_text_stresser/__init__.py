2967b17c112fb649d6c8845e678a9c0d833d8e06
Vuizur <Vuizur@users.noreply.github.com>
2022-04-30 10:24:24 +0200

    Fix structure more for publishing as a package

--- russian_text_stresser/__init__.py
+++ russian_text_stresser/__init__.py
@@ -1,3 +1,6 @@
-from .ebook_stresser import *
-from .text_stresser import *
-from .russian_dictionary import *
+# For relative imports to work in Python 3.6 
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+#from ebook_stresser import *
+#from text_stresser import *
+#from russian_dictionary import *
