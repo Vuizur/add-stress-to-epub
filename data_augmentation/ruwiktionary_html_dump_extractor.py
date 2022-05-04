67c0d259da05c1e9d2dcf8265b8063bf45d92f5f
Vuizur <Vuizur@users.noreply.github.com>
2022-05-04 11:44:40 +0200

    Fix ruwiktionary scraper

--- data_augmentation/ruwiktionary_html_dump_extractor.py
+++ data_augmentation/ruwiktionary_html_dump_extractor.py
@@ -1,16 +1,14 @@
-from distutils.command.build_scripts import first_line_re
 import os
-from pathlib import Path
 import json
 from bs4 import BeautifulSoup, PageElement
 import re
-# has to contain cyrillic, but not the characters only other slavic languages have
 
+# has to contain cyrillic, but not the characters only other slavic languages have
 
 def can_be_russian(text: str):
     text_lower = text.lower()
     return bool(re.search('[а-яА-Я]', text)) and "ў" not in text_lower and "ї" not in text_lower and "є" not in text_lower and \
-        "ћ" not in text_lower and "ђ" not in text_lower and "ґ" not in text_lower and "і" not in text_lower
+        "ћ" not in text_lower and "ђ" not in text_lower and "ґ" not in text_lower and "і" not in text_lower and "қ" not in text_lower
 
 
 def get_lemma(lemma_bold_paragraph) -> str:
@@ -31,11 +29,11 @@ def get_morph_table_words(morph_table) -> list[str]:
 
             words.extend(cell.get_text("|").split("|"))
 
-    return [word for word in words if word != "-" and word != "—"]
+    return [word.replace("буду/будешь… ", "") for word in words if word != "-" and word != "—"]
 
 
 def extract_stressed_words_from_section(section: PageElement) -> tuple[str, list[str]]:
-    lemma_p = section.find("p")
+    lemma_p = section.find("p", about=True)
     lemma = get_lemma(lemma_p.b)
 
     morpher_table = section.find("table", {"class": "morfotable"})
@@ -46,27 +44,36 @@ def extract_stressed_words_from_section(section: PageElement) -> tuple[str, list
     else:
         return (lemma, None)
 
+def section_contains_two_etymologies(section):
+    #h2 etc
+    return section.find("h2")
 
 def get_stressed_words_from_html(html: str) -> dict[str, list[str]]:
-    # print(html)
-    # lxml is supposed to be the fastest parser
-    #lemma: str = None
 
     soup = BeautifulSoup(html, "lxml")
-    # The HTML dump pages are structured differently from the online hosted version
+    # Important: The HTML dump pages are structured differently from the online hosted version
     inflection_dict = {}
     russian_h1 = soup.find("h1", id="Русский")
     if russian_h1 != None:
         for sibling in russian_h1.next_siblings:
             if sibling.name == "section":
-                section_res = extract_stressed_words_from_section(sibling)
-                if section_res == None:
-                    return None
+                if not section_contains_two_etymologies(sibling):
+                    section_res = extract_stressed_words_from_section(sibling)
+                    if section_res == None:
+                        return None
+                    else:
+                        lemma, inflections = section_res
+                        inflection_dict[lemma] = inflections
+                        return inflection_dict
                 else:
-                    lemma, inflections = section_res
-                    inflection_dict[lemma] = inflections
+                    for sibling in russian_h1.next_siblings:
+                        if sibling.name == "section":
+                            section_res = extract_stressed_words_from_section(sibling)
+                            if section_res == None:
+                                continue
+                            lemma, inflections = section_res
+                            inflection_dict[lemma] = inflections
                     return inflection_dict
-                
     else:
         return None
 
@@ -83,7 +90,6 @@ def extract_words_from_html_dump():
                 obj = json.loads(line)
                 name = obj["name"]
                 if can_be_russian(name):
-                    # TODO: Fix this so that it supports entries with different etymologies
                     try:
                         lemma_infl_dict = get_stressed_words_from_html(
                             obj["article_body"]["html"])
@@ -93,14 +99,15 @@ def extract_words_from_html_dump():
                                 if inflections != None:
                                     word_set.update(inflections)
                     except:
-                        print(f"PARSE ERROR for the word {name}")
+                        #print(f"PARSE ERROR for the word {name}")
+                        pass
                     
                     # if lemma != None:
                     #    word_dict[lemma] = inflections
                     # print(name)
                 i += 1
-                if i > 100:
-                    break
+                if i % 5000 == 0:
+                    print(i)
         with open("ruwiktionary_extracted_words.txt", "w", encoding="utf-8") as out:
             # json.dump(word_dict, out, ensure_ascii=False, indent=0)
             out.write("\n".join(word_set))
