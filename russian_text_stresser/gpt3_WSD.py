6ba9caa7e33f5f472b4332fc8826361d0beb0b55
Vuizur <Vuizur@users.noreply.github.com>
2022-11-08 22:01:10 +0100

    Add aspect info to word sense disambiguation

--- russian_text_stresser/gpt3_WSD.py
+++ russian_text_stresser/gpt3_WSD.py
@@ -54,6 +54,20 @@ class Entry:
     grammar_info: str
     IPA: str
 
+
+def get_pos_from_grammar_info(grammar_info: str) -> str:
+    return grammar_info.replace(",", " ").replace(";", " ").split(" ")[0]
+
+
+def get_aspect_from_grammar_info(grammar_info: str) -> str | None:
+    if "несовершенный вид" in grammar_info:
+        return "несовершенный"
+    elif "совершенный вид" in grammar_info:
+        return "совершенный"
+    else:
+        return None
+
+
 class RuWiktionary:
     def __init__(
         self, russian_wiktionary_json_path: str, database_path: str = "ruwiktionary.db"
@@ -65,7 +79,7 @@ class RuWiktionary:
             self.cursor = self.conn.cursor()
 
             self.cursor.execute(
-                "CREATE TABLE words (word_id INTEGER PRIMARY KEY, word TEXT, word_lower_unstressed TEXT, definitions TEXT, grammar_info TEXT, IPA TEXT)"
+                "CREATE TABLE words (word_id INTEGER PRIMARY KEY, word TEXT, word_lower_unstressed TEXT, definitions TEXT, grammar_info TEXT, IPA TEXT, pos TEXT, aspect TEXT)"
             )
             self.cursor.execute(
                 "CREATE TABLE inflections (inflection_id INTEGER PRIMARY KEY, word_id INTEGER, inflection TEXT, inflection_lower_unstressed TEXT, FOREIGN KEY(word_id) REFERENCES words(word_id))"
@@ -73,14 +87,17 @@ class RuWiktionary:
             with open(russian_wiktionary_json_path, "r", encoding="utf-8") as f:
                 all_words = json.load(f)
                 for word in all_words:
+
                     self.cursor.execute(
-                        "INSERT INTO words (word, word_lower_unstressed, definitions, grammar_info, IPA) VALUES (?, ?, ?, ?, ?)",
+                        "INSERT INTO words (word, word_lower_unstressed, definitions, grammar_info, IPA, pos, aspect) VALUES (?, ?, ?, ?, ?, ?, ?)",
                         (
                             word["word"],
                             remove_yo(unaccentify(word["word"].lower())),
                             json.dumps(word["definitions"], ensure_ascii=False),
                             word["grammar_info"],
                             word["IPA"],
+                            get_pos_from_grammar_info(word["grammar_info"]),
+                            get_aspect_from_grammar_info(word["grammar_info"]),
                         ),
                     )
                     word_id = self.cursor.lastrowid
@@ -107,14 +124,26 @@ class RuWiktionary:
 
             self.cursor.execute("CREATE INDEX words_idx_0435a8e8 ON words(word_id)")
 
+            # self.cursor.execute("CREATE INDEX words_idx_aspect ON words(aspect)")
+
             self.cursor.execute(
-                """CREATE VIEW words_with_inflections AS SELECT words.word_id, words.word, words.definitions, words.grammar_info, words.IPA, GROUP_CONCAT(inflections.inflection, "|") AS inflections FROM words LEFT JOIN inflections ON words.word_id = inflections.word_id GROUP BY words.word_id"""
+                """CREATE VIEW words_with_inflections AS SELECT words.word_id, words.word, words.word_lower_unstressed, words.definitions, words.pos, words.grammar_info, words.IPA, words.aspect, GROUP_CONCAT(inflections.inflection, "|") AS inflections FROM words LEFT JOIN inflections ON words.word_id = inflections.word_id GROUP BY words.word_id"""
             )
 
             self.conn.commit()
         else:
             self.conn = sqlite3.connect(database_path)
             self.cursor = self.conn.cursor()
+
+    def get_words_where_aspect_matters(self) -> list[str]:
+        # Return all entries where to one word_lower_unstressed corresponds both a perfective and an imperfective verb
+
+        # SELECT wwi.word_lower_unstressed, wwi.aspect FROM words_with_inflections wwi GROUP BY word_lower_unstressed HAVING COUNT(DISTINCT aspect) > 1
+        # Execute it
+        self.cursor.execute(
+            "SELECT word_lower_unstressed FROM words_with_inflections GROUP BY word_lower_unstressed HAVING COUNT(DISTINCT aspect) > 1"
+        )
+        return [row[0] for row in self.cursor.fetchall()]
 
     def get_entries(self, word: str) -> list[Entry]:
         """Searches for all fitting entries for an (unstressed) word in the database and returns the words and its inflections"""
@@ -125,7 +154,9 @@ class RuWiktionary:
         rows = self.cursor.fetchall()
 
         # Remove all rows where the word is uppercase and the parameter word is lowercase
-        rows = [row for row in rows if not (row[1][0].isupper() and not word[0].isupper())]
+        rows = [
+            row for row in rows if not (row[1][0].isupper() and not word[0].isupper())
+        ]
 
         inflection_word_ids = self.get_entries_with_inflection(word)
         if inflection_word_ids is not None:
@@ -171,7 +202,9 @@ class RuWiktionary:
         # Remove rows with None as word
         rows = [row for row in rows if row[1] is not None and len(row[1]) > 0]
         # Remove all rows where the word is uppercase and the parameter word is lowercase
-        rows = [row for row in rows if not (row[1][0].isupper() and not word[0].isupper())]
+        rows = [
+            row for row in rows if not (row[1][0].isupper() and not word[0].isupper())
+        ]
 
         if len(rows) == 0:
             return None
@@ -190,11 +223,14 @@ class RuWiktionary:
     def __del__(self):
         self.conn.close()
 
+
 @dataclass
 class DisambiguationTask:
     """A list of disambiguation tasks"""
+
     generated_string: str
     choices: list[Entry]
+
 
 class WordSenseDisambiguator:
     def __init__(
@@ -209,6 +245,17 @@ class WordSenseDisambiguator:
             openai.api_key = f.readline().strip()
         self.russian_wiktionary = RuWiktionary(russian_wiktionary_json_path)
         self.disambiguation_tasks = []
+        self.words_where_aspect_matters = set(
+            self.russian_wiktionary.get_words_where_aspect_matters()
+        )
+
+    def get_cleaned_up_grammar_info(self, entry: Entry) -> str:
+        pos = get_pos_from_grammar_info(entry.grammar_info)
+        aspect = get_aspect_from_grammar_info(entry.grammar_info)
+        if remove_yo(unaccentify(entry.word.lower())) in self.words_where_aspect_matters and aspect is not None:
+            return f"{pos}, {aspect} вид"
+        else:
+            return pos
 
     @staticmethod
     def find_in_entry_matching_word(word: str, entry: Entry) -> list[str]:
@@ -221,7 +268,9 @@ class WordSenseDisambiguator:
             # for inflection in entry["inflections"] + [entry["word"]]
             for inflection in entry.inflections + [entry.word]
             if remove_yo(unaccentify(inflection.lower()))
-            == remove_yo(unaccentify(word.lower())) #and not (inflection[0].isupper and word[0].islower())
+            == remove_yo(
+                unaccentify(word.lower())
+            )  # and not (inflection[0].isupper and word[0].islower())
         ]
         return compatible_words
 
@@ -264,8 +313,10 @@ class WordSenseDisambiguator:
         else:
             options = "\n".join(
                 [
-                    f"{i + 1}. {entry.word} ({entry.grammar_info.replace(',', ' ').replace(';', ' ').split(' ')[0]}) - {entry.definitions[0].split('◆')[0].strip()}"
-                    for i, entry in enumerate(valid_entries) #if entry.definitions is not None and entry.definitions != []
+                    f"{i + 1}. {entry.word} ({self.get_cleaned_up_grammar_info(entry)}) - {entry.definitions[0].split('◆')[0].strip()}"
+                    for i, entry in enumerate(
+                        valid_entries
+                    )  # if entry.definitions is not None and entry.definitions != []
                 ]
             )
 
@@ -280,7 +331,8 @@ class WordSenseDisambiguator:
                 DisambiguationTask(
                     generated_string=question,
                     choices=valid_entries,
-                ))
+                )
+            )
             print(question)
 
     def detect_and_fix_missing_stressed_words(self, text: str) -> str:
@@ -299,16 +351,23 @@ class WordSenseDisambiguator:
 
     def export_disambiguation_tasks_to_json(self):
         with open("disambiguation_tasks.json", "w", encoding="utf-8") as f:
-            json.dump(self.disambiguation_tasks, f, ensure_ascii=False, default=pydantic_encoder, indent=4)
+            json.dump(
+                self.disambiguation_tasks,
+                f,
+                ensure_ascii=False,
+                default=pydantic_encoder,
+                indent=4,
+            )
 
 
 def generate_tasks_file():
     pass
 
+
 if __name__ == "__main__":
-    #hd = HTMLDumpParser(None, "ruwiktdata_int.json", "ruwiktdata_cleaned.json")
-    #hd.clean_entries()
-    #quit()
+    # hd = HTMLDumpParser(None, "ruwiktdata_int.json", "ruwiktdata_cleaned.json")
+    # hd.clean_entries()
+    # quit()
     # rw = RuWiktionary("ruwiktdata_cleaned.json")
     ##word = "леса"
     # word = "лица"
@@ -321,15 +380,15 @@ if __name__ == "__main__":
     # print(f"Is impossible to disambiguate: {wsd.is_impossible_to_disambiguate(word, entries)}")
     # print(matchwords)
 
-    #wsd = WordSenseDisambiguator()
-    #print(wsd.russian_wiktionary.get_entries("нельзя"))
-    #print(wsd.disambiguate("замок", "В замке было очень темно."))
-    #print(
+    # wsd = WordSenseDisambiguator()
+    # print(wsd.russian_wiktionary.get_entries("нельзя"))
+    # print(wsd.disambiguate("замок", "В замке было очень темно."))
+    # print(
     #    wsd.disambiguate(
     #        "потом",
     #        "С потом переносятся феромоны и множество биологически активных веществ.",
     #    )
-    #)
+    # )
 
     # Load disambiguation_text.txt
     with open("disambiguation_text3.txt", "r", encoding="utf-8") as f:
