8fa39da4c18ada7cac576a64bcbe141aa50d948c
Vuizur <Vuizur@users.noreply.github.com>
2023-02-08 14:03:11 +0100

    Add additional dependencies + fix dict download

--- russian_text_stresser/download_db.py
+++ russian_text_stresser/download_db.py
@@ -1,4 +1,4 @@
 from russian_dictionary import RussianDictionary
 
 
-rd = RussianDictionary() # This should download the database if it doesn't exist
+rd = RussianDictionary("russian_dict.db") # This should download the database if it doesn't exist
