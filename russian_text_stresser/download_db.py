293613b4219c0edb75195f37055d7b4235daba99
Vuizur <Vuizur@users.noreply.github.com>
2023-04-27 14:46:23 +0200

    Try to fix CI download

--- russian_text_stresser/download_db.py
+++ russian_text_stresser/download_db.py
@@ -2,5 +2,5 @@ from russian_dictionary import RussianDictionary
 
 
 rd = RussianDictionary(
-    "russian_dict.db"
+    "russian_dict.db", None
 )  # This should download the database if it doesn't exist
