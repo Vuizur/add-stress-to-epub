7af88cd9875f237c8a2281dd1a67c7e2f7e4f0e7
Vuizur <Vuizur@users.noreply.github.com>
2023-08-16 12:20:56 +0200

    Host server with uvicorn and move folder

--- russian_text_stresser/app.py
+++ russian_text_stresser/app.py
@@ -1,4 +1,5 @@
-from litestar import Litestar, get, post
+import uvicorn
+from litestar import Litestar, post
 from russian_text_stresser.text_stresser import RussianTextStresser
 from litestar.config.cors import CORSConfig
 from pydantic import BaseModel
@@ -15,3 +16,6 @@ async def stress_text(data: StressRequest) -> str:
     return stresser.stress_text(data.text)
 
 app = Litestar([stress_text], cors_config=cors_config)
+
+if __name__ == "__main__":
+    uvicorn.run(app)
